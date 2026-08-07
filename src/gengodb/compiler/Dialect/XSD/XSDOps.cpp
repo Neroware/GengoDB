403558c822efe4fa5856496260fe89e18f12ebf4
Raoul Zebisch <raoul.zebisch@uni-a.de>
2026-08-07 13:37:44 +0000

    Removed dead .h/.cpp files

--- src/gengodb/compiler/Dialect/XSD/XSDOps.cpp
+++
@@ -1,8 +0,0 @@
-#include "gengodb/compiler/Dialect/XSD/XSDOps.h"
-
-#include "gengodb/compiler/Dialect/XSD/XSDDialect.h"
-
-#include "mlir/IR/OpImplementation.h"
-
-#define GET_OP_CLASSES
-#include "gengodb/compiler/Dialect/XSD/XSDOps.cpp.inc"
--- src/gengodb/compiler/Dialect/XSD/XSDOpsEnums.cpp
+++
@@ -1,5 +0,0 @@
-#include "gengodb/compiler/Dialect/XSD/XSDOps.h"
-#include "mlir/IR/OpImplementation.h"
-
-#define GET_OP_CLASSES
-#include "gengodb/compiler/Dialect/XSD/XSDOpsEnums.cpp.inc"
