4df29084d29bfb64035f1b04068a83d8daced61e
Raoul Zebisch <raoul.zebisch@uni-a.de>
2026-08-28 19:00:54 +0000

    Fixed bug in limit_basic.mlir, added join nullkey tests

--- tools/gengodb/oracle/gpm_queries.py
+++ tools/gengodb/oracle/gpm_queries.py
@@ -92,7 +92,7 @@ SELECT ?person ?age ?size WHERE {
 
 "filter/filter_literal_le_excludes.mlir": EX + "SELECT ?person ?age WHERE { ?person ex:age ?age . FILTER(?age <= 25) }",
 
-"filter/limit_basic.mlir": EX + "SELECT ?who ?what WHERE { ?who ex:drinks ?what } LIMIT 5",
+"filter/limit_basic.mlir": EX + "SELECT ?who ?what WHERE { ?who ex:drinks ?what } ORDER BY ?who ?what LIMIT 5",
 
 "optional/optional_bound.mlir": EX + "SELECT ?person ?food ?cup WHERE { ?person ex:eats ?food . OPTIONAL { ?person ex:cup ?cup } }",
 
